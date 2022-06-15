823c2d6cfd513daa68c93dab975002204e7ce565
Andrew Gomez <drewgomez209@gmail.com>
2022-06-15 07:27:15 -0700

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,40 @@
 // STRETCH
+
+// BUILT FIRST, DUMMY DATA
+const cars = [
+    {
+        vin: "1234567890123",
+        make: "toyota",
+        model: "prius",
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: "1234567890123",
+        make: "toyota",
+        model: "corolla",
+        mileage: 115000,
+        title: 'salvaged',
+    },
+    {
+        vin: "1234567890123",
+        make: "ford",
+        model: "focus",
+        mileage: 15000,
+        title: 'clean',
+    },
+
+]
+
+// exports.seed = function (knex) {
+//     return knex('cars')
+//         .truncate().then(() => {
+//             return knex('cars').insert(cars)
+//         })
+// }
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
